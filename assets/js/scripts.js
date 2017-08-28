8564f9b9cbb93157d9ff0527be2dc24618c99846
Lani Asep Sutisna <lani.asep@lawencon.com>
2017-08-28 11:44:11 +0700

    Update homepage

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,9 @@
 // Owlcarosoul Homepage
-$('.slider-homepage').owlCarousel({
+var url = window.location.pathname;
+$('.slider').owlCarousel({
     margin:10,
     nav:true,
-    navText: ["<img src='http://gettimee.ekuiva.com/wp-content/uploads/2017/08/left.png'>","<img src='http://gettimee.ekuiva.com/wp-content/uploads/2017/08/right.png'>"],
+    navText: ["<img src='" + url + "assets/images/left.png'>", "<img src='" + url + "assets/images/right.png'>"],
     responsive:{
         0:{
             items: 1,
@@ -16,49 +17,4 @@ $('.slider-homepage').owlCarousel({
             items:1
         }
     }
-});
-
-// Scrolling Smooth
-$(document).ready(function() { 
-    $('#nav-down').click(function(){
-        $('html, body').animate({
-            scrollTop: $( $(this).attr('href') ).offset().top
-        }, 800);
-        return false;
-    });
-    $('#nav-down-choose').click(function(){
-        $('html, body').animate({
-            scrollTop: $( $(this).attr('href') ).offset().top
-        }, 800);
-        return false;
-    });
-    $('#nav-down-gettimee-fitur').click(function(){
-        $('html, body').animate({
-            scrollTop: $( $(this).attr('href') ).offset().top
-        }, 800);
-        return false;
-    });
-    $('#nav-down-choose').click(function(){
-        $('html, body').animate({
-            scrollTop: $( $(this).attr('href') ).offset().top
-        }, 800);
-        return false;
-    });
-    $('#nav-down-contact').click(function(){
-        $('html, body').animate({
-            scrollTop: $( $(this).attr('href') ).offset().top
-        }, 800);
-        return false;
-    });
-});
-
-// Validator Number
-$(document).on("input", ".number", function () {
-    this.value = this.value.replace(/[^\d\+]/g, '');
-});
-
-// Tabs Mobile Accordion (Untuk Membedakan Warna Ketika Aktif)
-$('#accordion .card').on("click", function () {
-    $(this).siblings().find(".panel-clr").removeClass("on");
-    $(this).find(".panel-clr").toggleClass("on");
 });
